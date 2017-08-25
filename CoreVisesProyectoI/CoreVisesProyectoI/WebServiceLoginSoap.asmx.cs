8e468532ecc553221b5f6cce68337d6e5f2bbaa3
Strik15 <31193313+Strik15@users.noreply.github.com>
2017-08-24 22:02:04 -0600

    Agrega la clase RSA y sus referencias

--- CoreVisesProyectoI/CoreVisesProyectoI/WebServiceLoginSoap.asmx.cs
+++ CoreVisesProyectoI/CoreVisesProyectoI/WebServiceLoginSoap.asmx.cs
@@ -6,8 +6,7 @@ using System.Web.Services;
 using DOMAIN;
 using BUSINESS;
 
-namespace CoreVisesProyectoI
-{
+namespace CoreVisesProyectoI{
     /// <summary>
     /// Summary description for WebServiceLoginSoap
     /// </summary>
@@ -16,27 +15,36 @@ namespace CoreVisesProyectoI
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
     // [System.Web.Script.Services.ScriptService]
-    public class WebServiceLoginSoap : System.Web.Services.WebService
-    {
+    public class WebServiceLoginSoap : System.Web.Services.WebService{
 
         [WebMethod]
-        public string HelloWorld()
-        {
+        public string HelloWorld() { 
             return "Hello World";
-        }
+        }//End Helloworld
 
         [WebMethod]
-        public void LoginClient(String mail, String pass)
-        {
+        public void LoginClient(String mail, String pass){
             ClientBusiness cb = new ClientBusiness();
             Boolean verify;
-           // verify = cb.s
+            // verify = cb.s
 
-        }
+            /* Ejemplo de como instanciar y usar RSA            
+               
+            RSA rsa = new RSA(); lo instancia
+               
+            byte[] nameBytes = rsa.DecryptText(name, rsa.PrivateKey); name es la variable encriptada que recibe por parametros de tipo string
+
+            byte[] phoneBytes = rsa.DecryptText(phone, rsa.PrivateKey);  phone es la variable encriptada que recibe por parametros de tipo string
+
+            string nameDecrypted = Encoding.ASCII.GetString(nameBytes); Son los datos ya desencriptados de la variable name
+
+            string phoneDecrypted = Encoding.ASCII.GetString(phoneBytes); Son los datos ya desencriptados de la variable phone */
+        }//End LoginClient
 
         [WebMethod]
         public void RegisterClient() {
-        }
+        }//End RegisterClient
 
-    }
-}
+    }//End class WebServiceLoginSoap
+
+}//End namespace CoreVisesProyectoI
--- CoreVisesProyectoI/UTIL/RSA.cs
+++ CoreVisesProyectoI/UTIL/RSA.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UTIL{
     public class RSA{
         //Attributes
+        private string privateKey;
+        private string publicKey;
+
+        public RSA(){
+            this.privateKey = "<xml><BitStrength>512</BitStrength><RSAKeyValue><Modulus>8XotGiYuczvsRjVCtCKAf7DJ9IWFo6dzGAKnrXtvRxT5LwAHxxWGAdfsvl5pWCLxS9VUjR/FaneMAoGMH1gP9w==</Modulus><Exponent>AQAB</Exponent><P>/pEhmXDo/dWNYpTwNSLdeNiqF9CFORddKmnVrMfndjM=</P><Q>8tYuR1VeQ5pGVqrApmcoA0YlXVCr+8zpeHRiJn8Tky0=</Q><DP>GoyiV3FqK0cBZt2nq7MGLBtUqgUqT8j7A3hkO4/huPc=</DP><DQ>3pGGGyVYX3ewDlE0f4++sbmCF5QP/ZqjRh7NsuD37QU=</DQ><InverseQ>wd4u6p6Xbjm5k9qbxjuTdbiaQNz7Cz+1X2oYvJPtal4=</InverseQ><D>c4yDCIA8uedyqCXURV49Htz5xogd6rG14Udj9R+bn+b7UhJ4l2ocZcKc4YEq2fY+m8yznHILtiqxZSvhx6a8AQ==</D></RSAKeyValue></xml>";
+            this.publicKey = "<RSAKeyValue><Modulus>8XotGiYuczvsRjVCtCKAf7DJ9IWFo6dzGAKnrXtvRxT5LwAHxxWGAdfsvl5pWCLxS9VUjR/FaneMAoGMH1gP9w==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
+        }//End costruct default
+
+        public byte[] EncryptText(string text, string xmlPublic){
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(1024);
+            RSA.FromXmlString(xmlPublic);
+            byte[] encryptedData = RSA.Encrypt(Encoding.ASCII.GetBytes(text), true);
+            return encryptedData;
+        }//End EncryptText
+
+        public byte[] DecryptText(string encryptedText, string xmlPrivate){
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(1024);
+            RSA.FromXmlString(xmlPrivate);
+            byte[] decryptData = RSA.Decrypt(Convert.FromBase64String(encryptedText), true);
+            return decryptData;
+        }//End DecryptText
+
+        public string PrivateKey{
+            get{
+                return this.privateKey;
+            }//End get
+            set {
+                this.privateKey = value;
+            }//End set
+        }//End PrivateKey
+
+        public string PublicKey{
+            get{
+                return this.publicKey;
+            }//End get
+            set{
+                this.publicKey = value;
+            }//End set
+        }//End PublicKey
 
     }//End class RSA
 
