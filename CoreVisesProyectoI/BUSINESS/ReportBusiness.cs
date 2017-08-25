402b0fda28e6edded8bdfa33dfe8046663152efb
Strik15 <31193313+Strik15@users.noreply.github.com>
2017-08-25 11:16:47 -0600

    Termina las clases ReportData y ReportBusiness

--- CoreVisesProyectoI/BUSINESS/ReportBusiness.cs
+++ CoreVisesProyectoI/BUSINESS/ReportBusiness.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using DATA;
+using DOMAIN;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UTIL;
 
-namespace BUSINESS
-{
-    class ReportBusiness
-    {
-    }
-}
+namespace BUSINESS{
+    public class ReportBusiness{
+        //Attributes
+        private ReportData rd;
+
+        public Boolean insertReport(float totalIncome, float totalExpenses, string dateStart, string dateEnd){
+
+            RSA rsa = new RSA();
+
+            byte[] totalIncomeBytes = rsa.DecryptText(totalIncome.ToString(), rsa.PrivateKey);
+            byte[] totalExpensesBytes = rsa.DecryptText(totalExpenses.ToString(), rsa.PrivateKey);
+            byte[] dateStartBytes = rsa.DecryptText(dateStart, rsa.PrivateKey);
+            byte[] dateEndBytes = rsa.DecryptText(dateEnd, rsa.PrivateKey);
+
+            string totalIncomeDecrypted = Encoding.ASCII.GetString(totalIncomeBytes);
+            string totalExpensesDecrypted = Encoding.ASCII.GetString(totalExpensesBytes);
+            string dateStartDecrypted = Encoding.ASCII.GetString(dateStartBytes);
+            string dateEndDecrypted = Encoding.ASCII.GetString(dateEndBytes);
+
+            return rd.insertReport(float.Parse(totalIncomeDecrypted), float.Parse(totalExpensesDecrypted), dateStartDecrypted, dateEndDecrypted);
+
+        }//end insertReport
+
+        public List<Report> selectClient(){
+            return rd.selectReport();
+        }//end selectClient
+
+    }//End class ReportBusiness
+
+}//End namespace BUSINESS
--- CoreVisesProyectoI/DATA/ReportData.cs
+++ CoreVisesProyectoI/DATA/ReportData.cs
@@ -10,8 +10,7 @@ using System.Threading.Tasks;
 namespace DATA{
     public class ReportData{
 
-        public Boolean insertReport(float totalIncome, float totalExpenses, String dateStart, String dateEnd)
-        {
+        public Boolean insertReport(float totalIncome, float totalExpenses, String dateStart, String dateEnd){
             Boolean ret = false;
             try{
 
@@ -25,11 +24,11 @@ namespace DATA{
 
             }catch (Exception error){
                 ret = false;
-            }//catch
+            }//End try-catch (Exception error)
             return ret;
         }//end insertClient
 
-        public List<Report> selectClient(){
+        public List<Report> selectReport(){
 
             MongoClient mc = new MongoClient("mongodb://aplicada:aplicada@ds139428.mlab.com:39428/info_aplicada_ucr");
             MongoServer ms = mc.GetServer();
@@ -38,66 +37,7 @@ namespace DATA{
             MongoCollection collection = db.GetCollection<Report>("Report");
             return collection.FindAllAs<Report>().ToList<Report>(); // select del doc cliente y lo almacena en una lista a retornar de tipo cliente
 
-        }//end insertClient
-
-        public Boolean loginClient(String mail, String pass){
-            Boolean ret = false;
-            try{
-                MongoClient mc = new MongoClient("mongodb://aplicada:aplicada@ds139428.mlab.com:39428/info_aplicada_ucr");
-                MongoServer ms = mc.GetServer();
-                MongoDatabase db = ms.GetDatabase("info_aplicada_ucr");
-                ms.Connect();
-                MongoCollection collection = db.GetCollection<Report>("Report");
-                //var query = Query.EQ("mail", mail);
-                //   var query = Query.EQ("mail", mail, "pass", pass);
-                // "
-                //   List<Client> client = collection.FindAs
-
-                //       if (client.pass.equalsIgnoreCase(pass)) { }
-
-                ret = true;
-            }catch (Exception error){
-                ret = false;
-            }
-            return ret;
-        }//end insertClient
-
-        public Boolean deleteClient(String name){
-            Boolean ret = false;
-            try{
-                MongoClient mc = new MongoClient("mongodb://aplicada:aplicada@ds139428.mlab.com:39428/info_aplicada_ucr");
-                MongoServer ms = mc.GetServer();
-                MongoDatabase db = ms.GetDatabase("info_aplicada_ucr");
-                ms.Connect();
-                MongoCollection collection = db.GetCollection<Report>("Report");
-                var query = Query.EQ("name", name); // comparacion para hacer la eliminacion del documento de la bd
-                collection.Remove(query);
-                ret = true;
-            }catch (Exception error){
-                ret = true;
-            }//catch
-            return ret;
-        }//end deleteClient
-
-        public Boolean updateClient(String name, String mail, String pass){
-            Boolean ret = false;
-            try {
-                MongoClient mc = new MongoClient("mongodb://aplicada:aplicada@ds139428.mlab.com:39428/info_aplicada_ucr");
-                MongoServer ms = mc.GetServer();
-                MongoDatabase db = ms.GetDatabase("info_aplicada_ucr");
-                ms.Connect();
-                MongoCollection collection = db.GetCollection<Report>("Report");
-                var query = Query.EQ("name", name); // comparacion para hacer la eliminacion del documento de la bd                                
-                var report = collection.FindOneAs<Report>(query);
-                report.mail = mail;
-                report.pass = pass;
-                collection.Save(report);
-                ret = true;
-            }catch (Exception error){
-                ret = true;
-            }//catch
-            return ret;
-        }//end updateClient
+        }//end selectReport 
 
     }//End class ReportData
 
