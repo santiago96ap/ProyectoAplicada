ab70def81d3eb40f53638fccf3db27dbab50cd0d
JEstiben <31193316+JEstiben@users.noreply.github.com>
2017-08-25 12:03:29 -0600

    Codigo en CustomerPurchaseBusiness y CustomerPurchaseData

--- CoreVisesProyectoI/BUSINESS/CustomerPurchaseBusiness.cs
+++ CoreVisesProyectoI/BUSINESS/CustomerPurchaseBusiness.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using DATA;
+using DOMAIN;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UTIL;
 
 namespace BUSINESS
 {
-    class CustomerPurchaseBusiness
+    public class CustomerPurchaseBusiness
     {
+
+        private CustomerPurchaseData customerPurchaseData;
+
+        public Boolean insertCustomerPurchase(String clientName, String productName, int quantity, int totalPay)
+        {
+
+            RSA rsa = new RSA();
+
+            byte[] clientNameBytes = rsa.DecryptText(clientName, rsa.PrivateKey);
+            byte[] productNameBytes = rsa.DecryptText(productName, rsa.PrivateKey);
+            byte[] quantityBytes = rsa.DecryptText(quantity.ToString(), rsa.PrivateKey);
+            byte[] totalPayBytes = rsa.DecryptText(totalPay.ToString(), rsa.PrivateKey);
+
+            string clietNameDecrypted = Encoding.ASCII.GetString(clientNameBytes);
+            string productNameDecrypted = Encoding.ASCII.GetString(productNameBytes);
+            string quantityDecrypted = Encoding.ASCII.GetString(quantityBytes);
+            string totalPayDecrypted = Encoding.ASCII.GetString(totalPayBytes);
+
+            return customerPurchaseData.insertCustomerPurchase(clietNameDecrypted, productNameDecrypted, int.Parse(quantityDecrypted), int.Parse(totalPayDecrypted));
+
+        }//end insertCustomerPurchase
+
+        public List<CustomerPurchase> selectCustomerPurchase()
+        {
+            return customerPurchaseData.selectCustomerPurchase();
+        }//end insertCustomerPurchase
+
+        public Boolean deleteCustomerPurchase(String clientName)
+        {
+            return customerPurchaseData.deleteCustomerPurchase(clientName);
+        }//end deleteCustomerPurchase
+
+        public Boolean updateCustomerPurchase(String clientName, String productName, int quantity, int totalPay)
+        {
+            return customerPurchaseData.updateCustomerPurchase(clientName, productName, quantity, totalPay);
+        }//end updateCustomerPurchase
+
     }//class
 }//namespace
--- CoreVisesProyectoI/DATA/CustomerPurchaseData.cs
+++ CoreVisesProyectoI/DATA/CustomerPurchaseData.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DOMAIN;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,88 @@ using System.Threading.Tasks;
 
 namespace DATA
 {
-    class CustomerPurchaseData
+    public class CustomerPurchaseData
     {
+        public Boolean insertCustomerPurchase(String clientName, String productName, int quantity, int totalPay)
+        {
+
+            Boolean ret = false; // return variable
+            try
+            {
+                MongoClient mc = new MongoClient("mongodb://aplicada:aplicada@ds139428.mlab.com:39428/info_aplicada_ucr");
+                MongoServer ms = mc.GetServer();
+                MongoDatabase db = ms.GetDatabase("info_aplicada_ucr");
+                var customerPurchase = new CustomerPurchase(clientName, productName, quantity, totalPay);
+                MongoCollection collection = db.GetCollection<CustomerPurchase>("Client_Report");
+                collection.Insert<CustomerPurchase>(customerPurchase);
+                ret = true;
+            }//try
+            catch (Exception error)
+            {
+                ret = false;
+            }//catch
+            return ret;
+
+        }//insertCustomerPurchase
+
+        public List<CustomerPurchase> selectCustomerPurchase()
+        {
+
+            MongoClient mc = new MongoClient("mongodb://aplicada:aplicada@ds139428.mlab.com:39428/info_aplicada_ucr");
+            MongoServer ms = mc.GetServer();
+            MongoDatabase db = ms.GetDatabase("info_aplicada_ucr");
+            ms.Connect();
+            MongoCollection collection = db.GetCollection<CustomerPurchase>("Client_Report");
+            return collection.FindAllAs<CustomerPurchase>().ToList<CustomerPurchase>(); // select del CustomerPurchase y lo almacena en una lista a retornar de tipo CustomerPurchase
+
+        }//insertCustomerPurchase
+
+        public Boolean deleteCustomerPurchase(String clientName)
+        {
+            Boolean ret = false;// return variable
+            try
+            {
+                MongoClient mc = new MongoClient("mongodb://aplicada:aplicada@ds139428.mlab.com:39428/info_aplicada_ucr");
+                MongoServer ms = mc.GetServer();
+                MongoDatabase db = ms.GetDatabase("info_aplicada_ucr");
+                ms.Connect();
+                MongoCollection collection = db.GetCollection<CustomerPurchase>("Client_Report");
+                var query = Query.EQ("clientName", clientName); // comparacion para hacer la eliminacion del documento de la bd
+                collection.Remove(query);
+                ret = true;
+            }//try
+            catch (Exception error)
+            {
+                ret = true;
+            }//catch
+            return ret;
+        }//end deleteCustomerPurchase
+
+        public Boolean updateCustomerPurchase(String clientName, String productName, int quantity, int totalPay)
+        {
+            Boolean ret = false;// return variable
+            try
+            {
+                MongoClient mc = new MongoClient("mongodb://aplicada:aplicada@ds139428.mlab.com:39428/info_aplicada_ucr");
+                MongoServer ms = mc.GetServer();
+                MongoDatabase db = ms.GetDatabase("info_aplicada_ucr");
+                ms.Connect();
+                MongoCollection collection = db.GetCollection<CustomerPurchase>("Client_Report");
+                var query = Query.EQ("clientName", clientName); // comparacion para hacer el update del documento de la bd                                
+                var customerPurchase = collection.FindOneAs<CustomerPurchase>(query);
+                customerPurchase.clientName = clientName;
+                customerPurchase.productName = productName;
+                customerPurchase.quantity = quantity;
+                customerPurchase.totalPay = totalPay;
+                collection.Save(customerPurchase);
+                ret = true;
+            }//try
+            catch (Exception error)
+            {
+                ret = true;
+            }//catch
+            return ret;
+        }//end updateCustomerPurchase
+
     }//class
 }//namespace
