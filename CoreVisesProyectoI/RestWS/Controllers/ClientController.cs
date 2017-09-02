593cac0bab18aade892b354623fa7c009f4e822b
mcalvo96 <31193305+mcalvo96@users.noreply.github.com>
2017-09-02 16:48:38 -0600

    se agrego product client

--- CoreVisesProyectoI/RestWS/Controllers/ClientController.cs
+++ CoreVisesProyectoI/RestWS/Controllers/ClientController.cs
@@ -19,18 +19,24 @@ namespace RestWS.Controllers
 
 
         // GET api/values/5
-        public string Get(int id)
+        public string Get(string name)
         {
-            return "value";
+            return name;
         }
 
         // POST api/values        
         [HttpPost]
-        public Boolean Post([FromBody]String name, [FromBody] String mail, [FromBody]String pass, [FromBody] String card)
+        //public Boolean Post([FromBody]String name, [FromBody] String mail, [FromBody]String pass, [FromBody] String card)
+        public Boolean Post([FromBody]Client c)
         {
             ClientBusiness cb = new ClientBusiness();
-            return cb.insertClientRest(name,mail,pass,card);//
-            //cb.insertClientRest(c.name, c.mail, c.pass, c.card);
+            string[] lines = { c._id,c.name, c.mail, c.pass, c.card };
+            // WriteAllLines creates a file, writes a collection of strings to the file,
+            // and then closes the file.  You do NOT need to call Flush() or Close().
+            System.IO.File.WriteAllLines(@"C:\Users\mcalvo96\Documents\win 7\WriteLog.txt", lines);
+
+         //   return cb.insertClientRest(name,mail,pass,card);//
+            return cb.insertClientRest(c.name, c.mail, c.pass, c.card);
 
         }
 
