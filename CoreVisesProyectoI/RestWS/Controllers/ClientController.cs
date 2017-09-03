1c2012a1d3984fda81537d35a19d81f1aeea8ed7
JEstiben <31193316+JEstiben@users.noreply.github.com>
2017-09-03 09:13:45 -0600

    Cambio en el metodo post Client

--- CoreVisesProyectoI/RestWS/Controllers/ClientController.cs
+++ CoreVisesProyectoI/RestWS/Controllers/ClientController.cs
@@ -28,13 +28,9 @@ namespace RestWS.Controllers
         // POST api/values        
         [HttpPost]
         //public Boolean Post([FromBody]String name, [FromBody] String mail, [FromBody]String pass, [FromBody] String card)
-        public Boolean Post([FromBody]Client c)
+        public int Post([FromBody] Client c)
         {
             ClientBusiness cb = new ClientBusiness();
-            string[] lines = { c._id,c.name, c.mail, c.pass, c.card };
-            // WriteAllLines creates a file, writes a collection of strings to the file,
-            // and then closes the file.  You do NOT need to call Flush() or Close().
-            System.IO.File.WriteAllLines(@"C:\Users\mcalvo96\Documents\win 7\WriteLog.txt", lines);
 
          //   return cb.insertClientRest(name,mail,pass,card);//
             return cb.insertClientRest(c.name, c.mail, c.pass, c.card);
