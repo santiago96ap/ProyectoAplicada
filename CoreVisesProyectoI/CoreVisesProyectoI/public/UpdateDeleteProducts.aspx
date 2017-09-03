ca5406e822d826a5ad762d997823485c6712192f
Strik15 <31193313+Strik15@users.noreply.github.com>
2017-09-02 23:40:16 -0600

    Agrega la clase UpdateDeleteProduct y la eliminacion de productos

---
+++ CoreVisesProyectoI/CoreVisesProyectoI/public/UpdateDeleteProducts.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Title="" Language="C#" EnableEventValidation="false" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="UpdateDeleteProducts.aspx.cs" Inherits="CoreVisesProyectoI.UpdateDeleteProducts" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="content">
+    <div class="login">
+        <div class="main-agileits">
+            <div class="form-w3agile form1">
+                <h3>Register Products</h3>
+                <form id="form1" runat="server">
+                    <div class="key">
+                        <asp:DropDownList ID="DropDownList1" runat="server"  AutoPostBack="True" required="" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                        </asp:DropDownList>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="key">
+                        <asp:TextBox ID="tbCategoryUpdateDeleteProduct" value="category" type="text" name="category" runat="server" placeholder="Category" required=""></asp:TextBox>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="key">
+                        <asp:TextBox ID="tbPriceUpdateDeleteProduct" value="price" type="text" name="price" runat="server" placeholder="Price" required=""></asp:TextBox>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="key">
+                        <asp:TextBox ID="tbQuantityUpdateDeleteProduct" value="quantity" type="text" name="quantity" runat="server" placeholder="Quantity" required=""></asp:TextBox>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="key">
+                        <asp:TextBox ID="tbStateUpdateDeleteProduct" type="text" value="state" name="state" runat="server" placeholder="State" required=""></asp:TextBox>
+                        <div class="clearfix"></div>
+                    </div>
+                    &nbsp;&nbsp;&nbsp;<asp:Button ID="Button2" value="Submit" runat="server" Text="Update" OnClick="Button2_Click" />
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    &nbsp;&nbsp;&nbsp;&nbsp;<asp:Button ID="Button1" value="Submit" runat="server" Text="Delete" OnClick="Button1_Click"  />
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <br/><asp:Label ID="Label1" runat="server"></asp:Label>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+</asp:Content>
---
+++ CoreVisesProyectoI/CoreVisesProyectoI/public/UpdateDeleteProducts.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BUSINESS;
+using DOMAIN;
+
+namespace CoreVisesProyectoI{
+	public partial class UpdateDeleteProducts : System.Web.UI.Page{
+
+        //Attributes
+        private String nameAttendant = "";
+        private int attendantId = 0;
+        private String name = "";
+        private Product p;
+
+        protected void Page_Load(object sender, EventArgs e){
+            ProductBusiness productBusiness = new ProductBusiness();
+            if (IsPostBack){                
+                List<Product> product = productBusiness.selectProduct();
+                this.nameAttendant = DropDownList1.SelectedItem.ToString();
+                foreach (Product productActual in product){
+                    if (nameAttendant.Equals("Name: " + productActual.name + " Category:" + productActual.category + " Precie:" + productActual.price + " Quantity:" + productActual.quantity + " Status:" + productActual.status)){
+                        this.name = productActual.name;
+                        this.p = productActual;
+                    }//End if  if (nameAttendant.Equals("Name: " + productActual.name + " Category:" + productActual.category + " Precie:" + productActual.price + " Quantity:" + productActual.quantity + " Status:" + productActual.status))
+                }//End foreach (Product productActual in product)
+            }else {
+                DropDownList1.Items.Clear();
+                DropDownList1.Items.Add("Unselected");
+                List<Product> product = productBusiness.selectProduct();
+                foreach (var item in product){
+                    DropDownList1.Items.Add("Name: " + item.name + " Category:" + item.category + " Precie:" + item.price + " Quantity:" + item.quantity + " Status:" + item.status);
+                }//End foreach  (var item in product)
+                this.nameAttendant = DropDownList1.SelectedItem.ToString();
+                foreach (Product productActual in product){
+                    if (nameAttendant.Equals("Name: " + productActual.name + " Category:" + productActual.category + " Precie:" + productActual.price + " Quantity:" + productActual.quantity + " Status:" + productActual.status)){
+                        this.name = productActual.name;
+                        this.p = productActual;
+                    }//End if  if (nameAttendant.Equals("Name: " + productActual.name + " Category:" + productActual.category + " Precie:" + productActual.price + " Quantity:" + productActual.quantity + " Status:" + productActual.status))
+                }//End foreach (Product productActual in product)
+            }//End if-else(IsPostBack)
+
+        }//End Page_Load
+
+        protected void Button2_Click(object sender, EventArgs e){
+
+        }//End Button2_Click
+
+        protected void Button1_Click(object sender, EventArgs e){
+            ProductBusiness productBusiness = new ProductBusiness();
+            if (productBusiness.deleteProduct(this.name)) {
+                Label1.Text = "Successful elimination" + this.name;
+            }//End if (productBusiness.deleteProduct(this.nameAttendant))
+        }//End Button1_Click
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e){
+            this.nameAttendant = DropDownList1.SelectedItem.ToString();
+        }//End DropDownList1_SelectedIndexChanged
+
+    }//End class UpdateDeleteProducts
+
+}//End namespace CoreVisesProyectoI
